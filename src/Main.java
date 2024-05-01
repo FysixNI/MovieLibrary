69e92440ef06650ead4208e839498369aa732dbd
Ethan Tate <50052151@serc.ac.uk>
2024-05-01 15:01:46 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        UserInterface u1 = new UserInterface();
+        u1.showMainMenu();
+/*Movie m = new Movie("Rogue One", 134, Genre.SPACEADVENTURE);
+Movie m2 = new Movie("Elf", 96, Genre.COMEDY);
+Movie m3 = new Movie("Poltergeist",114, Genre.HORROR );
+
+MovieLibrary library = new MovieLibrary();
+library.addMovie(m);
+library.addMovie(m2);
+library.addMovie(m3);
+
+int size = library.movieCount();
+System.out.println(size);
+
+library.showMovies();*/
+
+
+
+
+
+
+
+    }
+}
---
+++ src/UserInterface.java
@@ -0,0 +1,157 @@
+public class UserInterface {
+
+    private InputReader reader;
+
+    private MovieLibrary library;
+
+    public UserInterface() {
+
+        this.reader = new InputReader();
+
+    }
+
+    public void showMainMenu() {
+
+        boolean quit = false;
+
+        while (quit = false) {
+
+            System.out.println("Please select an option: ");
+            System.out.println("1. add a new movie");
+            System.out.println("2. show all movies");
+            System.out.println("3. show movie count");
+            System.out.println("0. quit");
+
+            String userInput = reader.getInput();
+
+            switch (userInput) {
+
+                case "1":
+                    this.addMovie();
+                    break;
+                case"2":
+                    this.listMovies();
+                    break;
+                case "3":
+                    this.showMovieCount();
+                    break;
+                case"0":
+                    quit = true;
+                    break;
+                default:
+                    System.out.println("That was not a valid choice.");
+
+            }
+
+        }
+
+    }
+
+    public void addMovie() {
+
+        System.out.println("Please enter the title of the movie: ");
+        String title = reader.getInput();
+
+        System.out.println("Please enter the running time of the movie: ");
+        String userInput = reader.getInput();
+        int runningTime = 0;
+
+
+
+        try {
+
+            runningTime = Integer.parseInt(userInput);
+
+        }
+        catch (NumberFormatException exception) {
+
+            System.err.println("The input could not be parsed to an int.");
+            return;
+
+        }
+
+        Genre aGenre;
+
+        System.out.println("1. Comedy");
+        System.out.println("2. Thriller");
+        System.out.println("3. History");
+        System.out.println("4. Action");
+        System.out.println("5. Romance");
+        System.out.println("6. Historical");
+        System.out.println("7. Space Adventure");
+
+        userInput = reader.getInput();
+
+        int genreNumber = -1;
+
+        try {
+
+            genreNumber = Integer.parseInt(userInput);
+
+        }
+        catch (NumberFormatException exception) {
+
+            System.err.println("The input could not be parsed to an int.");
+            return;
+
+        }
+
+        if( genreNumber < 1 || genreNumber > 6) {
+
+            System.err.println("That was not a valid option.");
+            return;
+
+        }
+
+        Genre genre = Genre.COMEDY;
+
+        switch (genreNumber) {
+
+            case 1:
+                genre = Genre.COMEDY;
+                break;
+            case 2:
+                genre = Genre.THRILLER;
+                break;
+            case 3:
+                genre = Genre.HORROR;
+                break;
+            case 4:
+                genre = Genre.ACTION;
+                break;
+            case 5:
+                genre = Genre.ROMANCE;
+                break;
+            case 6:
+                genre = Genre.HISTORICAL;
+                break;
+            case 7:
+                genre = Genre.SPACEADVENTURE;
+                break;
+
+        }
+
+        Movie m = new Movie(title, runningTime, genre);
+        this.library.addMovie(m);
+
+
+    }
+
+    public void listMovies() {
+
+        this.library.showMovies();
+
+    }
+
+    public void showMovieCount() {
+
+        int count = this.library.movieCount();
+
+        System.out.println(count);
+
+    }
+
+
+
+
+}
